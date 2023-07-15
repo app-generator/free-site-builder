27ceb5b0fdfe370be700f1687340ba5e8ea5f815
MohammadMargub <margubstridhyatech@gmail.com>
2023-07-16 00:28:51 +0530

    Grid and Card API integration done

--- builder/src/main.ts
+++ builder/src/main.ts
@@ -1,74 +1,141 @@
-import './style.css'
-import { onDragStart, onDragEnd, onDragOver, onDrop, onClear, onSave, onRestore, setupGlobalEvents } from './dnd.ts'
+import "./style.css";
+import {
+  onDragStart,
+  onDragEnd,
+  onDragOver,
+  onDrop,
+  onClear,
+  onSave,
+  onRestore,
+  setupGlobalEvents,
+} from "./dnd.ts";
 
-//fetch('http://127.0.0.1:5000/kits/bs5/div.html') 
-//.then(response => response.text())               // response.text() has the component that needs to be saved in  
-//.then(text => console.log(text))                 // builder-components
-//.catch(error => console.error(error));
-
-// Using Promise syntax:
 function downloadComponents() {
+  return fetch("https://components-server.onrender.com/kits/bs5/")
+    .then((response) => response.text())
+    .then((response_raw) => {
+      let response_json = JSON.parse(response_raw);
 
-    //return fetch('http://localhost:5000/kits/bs5/')
-    return fetch('https://components-server.onrender.com/kits/bs5/')
-      .then(response => response.text())
-      .then( response_raw => {
+      let card_bas64 =
+        response_json["content"]["components"]["general"]["card.html"];
+      let card_component = atob(card_bas64);
+      let cardContainer =
+        document.getElementsByClassName("card-components")[0];
+      var div = document.createElement("div");
+      div.innerHTML = card_component.trim();
 
-        //console.log( response_raw );    
+      cardContainer.appendChild(div.firstChild);
 
-        let response_json = JSON.parse( response_raw );
+      //  For Grid
+      let grid2Rows_base64 =
+        response_json["content"]["components"]["grid"]["grid-2rows.html"];
+      let grid2Rows = atob(grid2Rows_base64);
+      console.log("grid-2rows", grid2Rows);
 
-        let component_base64 = response_json['content']['components']['general']['card.html'];
-        let component        = atob( component_base64 );
+      let grid3Rows_base64 =
+        response_json["content"]["components"]["grid"]["grid-3rows.html"];
+      let grid3Rows = atob(grid3Rows_base64);
+      console.log("grid-3rows", grid3Rows);
 
-        //console.log( component );
+      let grid4Rows_base64 =
+        response_json["content"]["components"]["grid"]["grid-4rows.html"];
+      let grid4Rows = atob(grid4Rows_base64);
+      console.log("grid-4rows", grid4Rows);
 
-        let componentsContainer = document.getElementsByClassName('builder-components')[0];
+      let gridContainer = document.getElementsByClassName("grid-components")[0];
 
-        var div = document.createElement('div');
-        div.innerHTML = component.trim();
+      var div2Rows = document.createElement("div");
+      div2Rows.innerHTML = grid2Rows.trim();
+      gridContainer.appendChild(div2Rows.firstChild);
 
-        componentsContainer.appendChild(<Node>div.firstChild);
+      var div3Rows = document.createElement("div");
+      div3Rows.innerHTML = grid3Rows.trim();
+      gridContainer.appendChild(div3Rows.firstChild);
 
-      })
-      .catch(error => console.error(error));
+      var div4Rows = document.createElement("div");
+      div4Rows.innerHTML = grid4Rows.trim();
+      gridContainer.appendChild(div4Rows.firstChild);
+
+      // For Footer
+      let footer_base64 =
+      response_json["content"]["components"] ['footers'] ['footer.html'];
+    let footer_component = atob(footer_base64);
+    console.warn('footer',footer_component);
+    
+    let footersContainer =
+      document.getElementsByClassName("footer-components")[0];
+    var div = document.createElement("div");
+    div.innerHTML = footer_component.trim();
+
+    footersContainer.appendChild(div.firstChild);
+
+
+    })
+    .catch((error) => console.error(error));
 }
 
-let builderContainer = document.querySelector('#layout')!.innerHTML;
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = builderContainer;
+document.addEventListener("DOMContentLoaded", () => {
+  let builderContainer = document.querySelector("#layout")!.innerHTML;
+  document.querySelector<HTMLDivElement>("#app")!.innerHTML = builderContainer;
 
-// SETUP Navigation
-document.querySelector('#action_clear')!.addEventListener('click', (event) => { onClear(event) });
-document.querySelector('#action_save')!.addEventListener('click', (event) => { onSave(event) });
-document.querySelector('#action_restore')!.addEventListener('click', (event) => { onRestore(event) });
-document.querySelector('#action_undo')!.addEventListener('click', (event) => { onRestore(event) });
+  // SETUP Navigation
+  document
+    .querySelector("#action_clear")
+    ?.addEventListener("click", (event) => {
+      onClear(event);
+    });
+  document.querySelector("#action_save")?.addEventListener("click", (event) => {
+    onSave(event);
+  });
+  document
+    .querySelector("#action_restore")
+    ?.addEventListener("click", (event) => {
+      onRestore(event);
+    });
+  document.querySelector("#action_undo")?.addEventListener("click", (event) => {
+    onRestore(event);
+  });
 
-// PULL Components 
-downloadComponents().then(misc)
+  // PULL Components
+  downloadComponents().then(misc);
 
-// SETUP Components
-function misc() {
-
-    let draggableElems = document.querySelectorAll('.draggable');
+  // SETUP Components
+  function misc() {
+    let draggableElems = document.querySelectorAll(".draggable");
 
     for (let i = 0; i < draggableElems.length; i++) {
-        draggableElems[i].addEventListener('dragstart', (event) => { onDragStart(event) });
-        draggableElems[i].addEventListener('dragend', (event) => { onDragEnd(event) });
+      draggableElems[i].addEventListener("dragstart", (event) => {
+        onDragStart(event);
+      });
+      draggableElems[i].addEventListener("dragend", (event) => {
+        onDragEnd(event);
+      });
     }
-}
+  }
 
-// SETUP Master DROP Zone
-document.querySelector('#dropzone')!.addEventListener('dragover', (event) => { onDragOver(event) });
-document.querySelector('#dropzone')!.addEventListener('drop', (event) => { onDrop(event) });
+  // SETUP Master DROP Zone
+  document.querySelector("#dropzone")?.addEventListener("dragover", (event) => {
+    onDragOver(event);
+  });
+  document.querySelector("#dropzone")?.addEventListener("drop", (event) => {
+    onDrop(event);
+  });
 
-// SETUP GRID Drop Zones
-let dropZones = document.getElementsByClassName('dropzone-elem');
-for (let i = 0; i < dropZones.length; i++) {
-    dropZones[i].addEventListener('dragover', (event) => { onDragOver(event) });
-    dropZones[i].addEventListener('dragend', (event) => { onDragEnd(event) });
-    dropZones[i].addEventListener('drop', (event) => { onDrop(event) });
-}
+  // SETUP GRID Drop Zones
+  let dropZones = document.getElementsByClassName("dropzone-elem");
+  for (let i = 0; i < dropZones.length; i++) {
+    dropZones[i].addEventListener("dragover", (event) => {
+      onDragOver(event);
+    });
+    dropZones[i].addEventListener("dragend", (event) => {
+      onDragEnd(event);
+    });
+    dropZones[i].addEventListener("drop", (event) => {
+      onDrop(event);
+    });
+  }
 
-onRestore(null);
+  onRestore(null);
 
-setupGlobalEvents();
+  setupGlobalEvents();
+});
