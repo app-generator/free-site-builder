efcea1d1a0fae2dde1175311ba64734c56b18115
elias <kompanipejper@gmail.com>
2023-07-17 17:59:15 +0900

    complete:dynamic-sidbar

--- builder/src/main.ts
+++ builder/src/main.ts
@@ -8,57 +8,63 @@ import { onDragStart, onDragEnd, onDragOver, onDrop, onClear, onSave, onRestore,
 
 // Using Promise syntax:
 function downloadComponents() {
-
+    let loading = document.querySelector('#overlay') as HTMLElement;
     //return fetch('http://localhost:5000/kits/bs5/')
-    return fetch('https://components-server.onrender.com/kits/bs5/')
-    .then(response => response.text())
-    .then( response_raw => {
+    let localStorageData = window.localStorage.getItem('components');
+    if (localStorageData) {
+      let localStorageParsedData = JSON.parse(<string>window.localStorage.getItem('components'));
+      return new Promise((resolve, reject) => {
+        // Simulating an asynchronous operation
+        resolve(drawComponents(localStorageParsedData));
+      });
+    } else {
+      loading.style.display = 'flex';
+      return fetch('https://components-server.onrender.com/kits/bs5/')
+        .then(response => response.text())
+        .then( response_raw => {
+          loading.style.display = 'none';
+          let response_json = JSON.parse( response_raw );
+          window.localStorage.setItem('components', JSON.stringify(response_json))
+          drawComponents(response_json);
+        })
+        .catch(error => console.error(error));
+    }
+}
 
-      //console.log( response_raw );    
+function drawComponents(response_json:any) {
+  let components = response_json['content']['components'];
+  let component = '<div class="accordion subaccordionCustom" id="subaccordionExample">';
+  for (let item in components) {
+    let subComponents = components[item];
+    let gridStr = '';
+    for (let subItem in subComponents) {
+      let component_grid_base64 = subComponents[subItem];
+      gridStr += atob( component_grid_base64 );
 
-      let response_json = JSON.parse( response_raw );
-
-      let components = response_json['content']['components'];
-      let component = '<div class="accordion subaccordionCustom" id="subaccordionExample">';
-
-      for (let item in components) {
-        let subComponents = components[item];
-        let gridStr = '';
-        for (let subItem in subComponents) {
-          let component_grid_base64 = subComponents[subItem];
-          gridStr += atob( component_grid_base64 );
-
-        }
-
-        var gridSize = Object.keys(subComponents).length;
-        component += `
-          <div class="accordion-item">
-          <h2 class="accordion-header" id="headingTwo2-`+item+`">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-              data-bs-target="#collapseTwo2-`+item+`" aria-expanded="false" aria-controls="collapseTwo2-`+item+`">
-              `+item+`
-              <span class="forNumbers">`+gridSize+`</span>
-            </button>
-          </h2>
-          <div id="collapseTwo2-`+item+`" class="accordion-collapse collapse" aria-labelledby="headingTwo2-`+item+`"
-            data-bs-parent="#subaccordionExample">
-            <div class="accordion-body">
-              `+gridStr+`
-            </div>
-          </div>
-        </div>`;
-      }
-      component        += '</div>';
-      
-      // console.log( component, "test" );
-      let componentsContainer = document.getElementsByClassName('components_contain')[0];
-
-      var div = document.createElement('div');
-      div.innerHTML = component.trim();
-
-      componentsContainer.appendChild(<Node>div);
-    })
-    .catch(error => console.error(error));
+    }
+    var gridSize = Object.keys(subComponents).length;
+    component += `
+      <div class="accordion-item">
+      <h2 class="accordion-header" id="headingTwo2-${item}">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+          data-bs-target="#collapseTwo2-${item}" aria-expanded="false" aria-controls="collapseTwo2-${item}">
+          ${item}
+          <span class="forNumbers">${gridSize}</span>
+        </button>
+      </h2>
+      <div id="collapseTwo2-${item}" class="accordion-collapse collapse" aria-labelledby="headingTwo2-${item}"
+        data-bs-parent="#subaccordionExample">
+        <div class="accordion-body">
+          ${gridStr}
+        </div>
+      </div>
+    </div>`;
+  }
+  component += '</div>';
+  let componentsContainer = document.getElementsByClassName('components_contain')[0];
+  var div = document.createElement('div');
+  div.innerHTML = component.trim();
+  componentsContainer.appendChild(<Node>div);
 }
 
 
