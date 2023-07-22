19dfdf6ed494035ae0fe7ad538d116bd79cabf93
App Generator <51070104+app-generator@users.noreply.github.com>
2023-07-22 09:51:23 +0300

    ZIP, improve error checking

--- builder/src/main.ts
+++ builder/src/main.ts
@@ -201,10 +201,13 @@ function deployToNetlify(siteName: string, netlifyToken: string): void {
   let indexhtmlContent = drawHTMLForDownload('dropzone', 'index.html');
   zip.file('index.html', indexhtmlContent);
   zip.file('assets/css/index.css', style);
-  for (let i = 0; i < currentPages.length; i++) {
-    let pageTab = currentPages[i].split('_@COL@_');
-    let htmlContent = drawHTMLForDownload('dropzone-'+pageTab[0], pageTab[1], pageTab[0]);
-    zip.file(pageTab[1], htmlContent);
+
+  if (currentPages) {
+    for (let i = 0; i < currentPages.length; i++) {
+      let pageTab = currentPages[i].split('_@COL@_');
+      let htmlContent = drawHTMLForDownload('dropzone-'+pageTab[0], pageTab[1], pageTab[0]);
+      zip.file(pageTab[1], htmlContent);
+    }
   }
 
   // Generate the zip file as a Blob
