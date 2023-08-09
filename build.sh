ac6ebaa9a75322002a836a859dcc1aac57fba569
App Generator <51070104+app-generator@users.noreply.github.com>
2023-08-09 12:20:53 +0300

    Added HOMEpage

--- build.sh
+++ build.sh
@@ -5,13 +5,17 @@ set -o errexit
 yarn  
 yarn build 
 
-# copy Index
-cp index-cdn.html dist/index.html 
+# Copy HOMEpage 
+cp homepage-min.html dist/index.html 
 
-# copy volt 
+# Copy bs5 
+mkdir dist/bootstrap
+cp index-cdn.html bootstrap/index.html 
+
+# Copy volt 
 mkdir dist/volt
 cp index-volt.html dist/volt/index.html 
 
-# copy pixel 
+# Copy pixel 
 mkdir dist/pixel
 cp index-pixel.html dist/pixel/index.html 
